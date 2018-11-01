7de940906a01af14122a0a49b8dd13039fbf2919
NAPOLEON039 <nihar.raote@gmail.com>
2018-11-01 19:25:33 +0530

    Added typing effect for home page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/projects">Projects</router-link>
@@ -10,5 +11,11 @@
 </template>
 
 <style>
+#app {
+  height: 100vh;
+  background-color: #eec0c6;
+background-image: linear-gradient(315deg, #eec0c6 0%, #7ee8fa 74%);
 
+
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <h1>This is the main page</h1>
+    <vue-typed-js :showCursor="false" :typeSpeed="40" :backSpeed="15" :smartBackspace="true" :loop="true" :strings="['Hey there 👋 ^1500', 'I am a Front-end web developer. ^500', 'I primarily work in Vuejs.', 'I work with Vanilla JS, CSS and HTML for simple projects.', 'But I choose Vuejs for big and complex ones. ^1000', 'I am learning back-end technologies like Node.', 'With the goal of becoming a Full-Stack web developer. ^1000', 'I have an eye for good design ^500 and create my own UI for my projects.', 'I always push my knowledge to its limits ^500 and try to squeeze in something new ^350 everyday.', 'That\'s how I am as a developer. ^2000']">
+      <h1 class="typing"></h1>
+    </vue-typed-js>
   </div>
 </template>
 
