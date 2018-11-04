11185e5d1a04455cd11cf38c696729dfb2017ae6
NAPOLEON039 <nihar.raote@gmail.com>
2018-11-04 19:09:10 +0530

    Improved UI for home page and added some copy for About page

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div id="nav">
       
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
       <router-link to="/projects">Projects</router-link>
     </div>
     <router-view/>
@@ -13,9 +13,5 @@
 <style>
 #app {
   height: 100vh;
-  background-color: #eec0c6;
-background-image: linear-gradient(315deg, #eec0c6 0%, #7ee8fa 74%);
-
-
 }
 </style>
